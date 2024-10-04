aa38cb0e04af5fb07d0f2e8aa46c9d63b52ed4bf
Gabriel Altay <gabriel.altay@gmail.com>
2024-10-03 23:40:22 -0400

    rename

--- scripts/write_nx_graph.py
+++ scripts/write_nx_graph.py
@@ -8,7 +8,7 @@ from ztnd.graphs import build_graph_from_ztnd_choices
 
 cache_path = Path("cache") / "2024-10-03-21-41-11"
 ztnd_choices = load_ztnd_choices(cache_path / "ztnd_choices.json")
-graph = build_graph_from_ztnd_czhoices(ztnd_choices)
+graph = build_graph_from_ztnd_choices(ztnd_choices)
 data = nx.node_link_data(graph)
 out_path = cache_path / "node_link_data.json"
 with out_path.open("w") as fp:
--- ztnd/graphs.py
+++ ztnd/graphs.py
@@ -5,12 +5,12 @@ from ztnd.generations import ZtndChoice
 
 def build_graph_from_ztnd_choices(ztnd_choices: list[ZtndChoice]) -> nx.DiGraph:
     """Build a graph from the generated tokens."""
-    G = nx.DiGraph()
+    graph = nx.DiGraph()
 
     for ztnd_choice in ztnd_choices:
         for ztnd_token in ztnd_choice.tokens:
             node = ztnd_token.get_node()
-            G.add_nodes_from(
+            graph.add_nodes_from(
                 [
                     (
                         node,
@@ -29,15 +29,15 @@ def build_graph_from_ztnd_choices(ztnd_choices: list[ZtndChoice]) -> nx.DiGraph:
                 ztnd_choice.tokens[ii].get_node(),
                 ztnd_choice.tokens[ii + 1].get_node(),
             )
-            if G.has_edge(node_lo, node_hi):
-                G[node_lo][node_hi]["weight"] += 1
-                G[node_lo][node_hi]["choice_ids"].append(ztnd_choice.choice_id)
+            if graph.has_edge(node_lo, node_hi):
+                graph[node_lo][node_hi]["weight"] += 1
+                graph[node_lo][node_hi]["choice_ids"].append(ztnd_choice.choice_id)
             else:
-                G.add_edge(
+                graph.add_edge(
                     node_lo,
                     node_hi,
                     weight=1,
                     choice_ids=[ztnd_choice.choice_id],
                 )
 
-    return G
+    return graph
